6bd22c4aee64bfe13064e2c782aba131acc48966
Yunir Salimzyanov <yunir.me@gmail.com>
2021-05-09 17:09:46 +0300

    Set predictable password for OracleDB

--- oracledb/work_with_db.sh
+++ oracledb/work_with_db.sh
@@ -3,3 +3,4 @@ set -euxo pipefail
 
 echo 'echo exit | sqlplus / as sysdba @/vagrant/oracle_tables_create' | sudo su - oracle
 echo 'echo exit | sqlplus / as sysdba @/vagrant/oracle_tables_insert' | sudo su - oracle
+echo '/home/oracle/setPassword.sh ranger' | sudo su - oracle
