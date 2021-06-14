78d524cb06d99ad1c42bf348d929068f4e355c11
Andrey Zavodov <andrey.zavodov@jetbrains.com>
2021-06-15 01:51:01 +0300

    Add python lib requirements and script with examples

---
+++ merge/test_connections.py
@@ -0,0 +1,53 @@
+import psycopg2
+from contextlib import closing
+
+import cx_Oracle
+
+import pymysql
+from pymysql.cursors import DictCursor
+
+from pymongo import MongoClient
+
+with closing(psycopg2.connect(
+        dbname='dwdb',
+        user='manager',
+        password='ranger',
+        host='localhost',
+        port=15432,
+)) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT * FROM student')
+        for row in cursor:
+            print(row)
+
+
+cx_Oracle.init_oracle_client(lib_dir='/Users/andrey.zavodov/Desktop/instantclient_19_8')
+conn_str = u'SYSTEM/ranger@localhost:1521/XE'
+conn = cx_Oracle.connect(conn_str)
+c = conn.cursor()
+c.execute(u'select * from employee')
+for row in c:
+    print(row[0], "-", row[1])
+conn.close()
+
+
+with closing(
+        pymysql.connect(
+            host='localhost',
+            port=13306,
+            user='root',
+            password='root',
+            db='dwdb',
+            cursorclass=DictCursor
+        )
+) as connection:
+    with connection.cursor() as cursor:
+        cursor.execute("select * from EMPLOYEE")
+        for row in cursor:
+            print(row)
+
+
+client = MongoClient('mongodb://manager:ranger@localhost:27017/dwdb')
+db = client.dwdb
+for employee in db.employee.find():
+    print(employee)
