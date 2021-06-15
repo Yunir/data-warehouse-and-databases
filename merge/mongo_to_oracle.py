50648dd56d74907115d9d7395285e27d314dec39
Andrey Zavodov <andrey.zavodov@jetbrains.com>
2021-06-15 04:31:52 +0300

    MongoToOracle create tables and alter Student table

--- merge/mongo_to_oracle.py
+++ merge/mongo_to_oracle.py
@@ -13,23 +13,65 @@ class MongoToOracle:
         self.oracle.autocommit = True
 
     def merge(self):
-        self.merge_employee()
         self.merge_hostel()
         self.merge_room()
-        self.merge_student()
         self.merge_tenant()
-
-    def merge_employee(self):
-        pass
-
-    def merge_hostel(self):
-        pass
-
-    def merge_room(self):
-        pass
+        self.merge_student()
 
     def merge_student(self):
-        pass
+        try:
+            self.oracle.cursor().execute("""
+                alter table student
+                ADD has_benefits VARCHAR2(10)
+            """)
+        except Exception:
+            pass
+
+    def merge_hostel(self):
+        try:
+            self.oracle.cursor().execute("""
+                CREATE TABLE HOSTEL(
+                    ID_hostel NUMBER(5) PRIMARY KEY CHECK(ID_hostel>=0),
+                    hostel_location VARCHAR2(1000),
+                    rooms_number NUMBER(5),
+                    claws VARCHAR2(10)
+                )
+            """)
+        except Exception:
+            pass
+
+    def merge_room(self):
+        try:
+            self.oracle.cursor().execute("""
+                CREATE TABLE ROOM(
+                    ID_room NUMBER(5) PRIMARY KEY CHECK(ID_room>=0),
+                    hostel NUMBER(5) NOT NULL CONSTRAINT room_hostel_fkey REFERENCES HOSTEL(ID_hostel),
+                    payment_amount NUMBER(5),
+                    room_number NUMBER(5),
+                    room_capacity NUMBER(5),
+                    livers_number NUMBER(5),
+                    disinfection_date DATE
+                )
+            """)
+        except Exception:
+            pass
 
     def merge_tenant(self):
-        pass
+        try:
+            self.oracle.cursor().execute("""
+                CREATE TABLE TENANT(
+                    ID_tenant NUMBER(5) PRIMARY KEY CHECK(ID_tenant>=0),
+                    student NUMBER(5) NOT NULL CONSTRAINT tenant_student_fkey REFERENCES STUDENT(ID_student),
+                    room NUMBER(5) NOT NULL CONSTRAINT tenant_room_fkey REFERENCES ROOM(ID_room),
+                    visit_period_entered DATE,
+                    visit_period_left DATE,
+                    warnings_count NUMBER(5),
+                    arrival_date DATE,
+                    check_out_date DATE
+                )
+            """)
+        except Exception:
+            pass
+
+mg = MongoToOracle()
+mg.merge()
